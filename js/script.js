e12189829ab5805b72a755bd827655df56a9d973
lee_c <gitleelee@gmail.com>
2023-11-11 19:35:04 -0800

    Modified button with a function that triggers the cat image to display and dispapear.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 var button = document.querySelector(".show-cat");
+
 var cat = document.querySelector(".cat");
 
-// console.log(button);
-// console.log(cat);
-
-// When user clicks on the button, function is triggered to show cat image
+//A function triggered to display and remove the cat image.
 button.addEventListener("click", function () {
-  cat.classList.add("show");
+  if (cat.classList.contains("show")) {
+    // console.log("Yes");
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    button.classList.add("disappear");
+  } else {
+    // console.log("No");
+    cat.classList.add("show");
+    button.innerText = "Shoo, cat!";
+    button.classList.remove("disappear");
+  }
 });
